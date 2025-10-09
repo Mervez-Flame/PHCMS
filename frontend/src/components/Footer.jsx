c2b5cdf0caa25da0c645e63bbb7217c053413787
Jason Mervin Mark - Igbani <mervez2006@gmail.com>
2025-10-09 14:40:06 +0100

    Fa Error Update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { assets } from '../assets/assets';
-import { FaFacebook } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaTwitter } from "react-icons/fa";
 import { BsLinkedin } from "react-icons/bs";
